ee6e0a242da040b20a6d29fa95751cfbb88474c9
Ben Garrett <code.by.ben@gmail.com>
2020-01-14 10:23:26 +1100

    Improved Connect() feedback +DateTime() +IsNew()

--- lib/database/database_test.go
+++ lib/database/database_test.go
@@ -1,7 +1,10 @@
 package database
 
 import (
+	"database/sql"
+	"fmt"
 	"testing"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
@@ -53,6 +56,61 @@ func TestUUID(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := UUID(tt.args.id); got != tt.want {
 				t.Errorf("UUID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDateTime(t *testing.T) {
+	type args struct {
+		value sql.RawBytes
+	}
+	now := time.Now()
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"empty", args{[]byte("")}, "?"},
+		{"nottime", args{[]byte("hello world")}, "?"},
+		{"invalid", args{[]byte("01-01-2000 00:00:00")}, "?"},
+		{"old", args{[]byte("2000-01-01T00:00:00Z")}, "01 Jan 2000  "},
+		{"new", args{[]byte(fmt.Sprintf("%v-01-01T00:00:00Z", now.Year()))}, "01 Jan 00:00 "},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DateTime(tt.args.value); got != tt.want {
+				t.Errorf("DateTime() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isNew(t *testing.T) {
+	type args struct {
+		deleted sql.RawBytes
+		updated sql.RawBytes
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{"new", args{[]byte("2006-01-02T15:04:05Z"), []byte("2006-01-02T15:04:05Z")}, true, false},
+		{"new+offset", args{[]byte("2006-01-02T15:04:06Z"), []byte("2006-01-02T15:04:05Z")}, true, false},
+		{"old del", args{[]byte("2016-01-02T15:04:05Z"), []byte("2006-01-02T15:04:05Z")}, false, false},
+		{"old upd", args{[]byte("2000-01-02T15:04:05Z"), []byte("2016-01-02T15:04:05Z")}, false, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := isNew(tt.args.deleted, tt.args.updated)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("isNew() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("isNew() = %v, want %v", got, tt.want)
 			}
 		})
 	}
