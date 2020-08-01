e63773b70e3dad42c0c856e269c0396b877d8ce1
Ben Garrett <code.by.ben@gmail.com>
2020-08-01 14:45:13 +1000

    made uuid val a const.

--- lib/archive/proof_test.go
+++ lib/archive/proof_test.go
@@ -5,6 +5,7 @@ import (
 )
 
 func TestExtract(t *testing.T) {
+	const uuid = "6ba7b814-9dad-11d1-80b4-00c04fd430c8"
 	type args struct {
 		archive  string
 		filename string
@@ -17,14 +18,14 @@ func TestExtract(t *testing.T) {
 	}{
 		{"empty", args{}, true},
 		{"error", args{testDir("demozoo/test.zip"), "test.zip", "xxxx"}, true},
-		{"7z", args{testDir("demozoo/test.7z"), "test.7z", "6ba7b814-9dad-11d1-80b4-00c04fd430c8"}, true},
-		{"tar", args{testDir("demozoo/test.tar"), "test.tar", "6ba7b814-9dad-11d1-80b4-00c04fd430c8"}, false},
-		{"tar.gz", args{testDir("demozoo/test.tar.gz"), "test.tar.gz", "6ba7b814-9dad-11d1-80b4-00c04fd430c8"}, false},
-		{"tar.bz2", args{testDir("demozoo/test.tar.bz2"), "test.tar.bz2", "6ba7b814-9dad-11d1-80b4-00c04fd430c8"}, false},
-		{"sz (snappy)", args{testDir("demozoo/test.tar.sz"), "test.tar.sz", "6ba7b814-9dad-11d1-80b4-00c04fd430c8"}, false},
-		{"xz", args{testDir("demozoo/test.tar.xz"), "test.tar.xz", "6ba7b814-9dad-11d1-80b4-00c04fd430c8"}, false},
-		{"zip", args{testDir("demozoo/test.zip"), "test.zip", "6ba7b814-9dad-11d1-80b4-00c04fd430c8"}, false},
-		{"zip.bz2", args{testDir("demozoo/test.bz2.zip"), "test.bz2.zip", "6ba7b814-9dad-11d1-80b4-00c04fd430c8"}, false},
+		{"7z", args{testDir("demozoo/test.7z"), "test.7z", uuid}, true},
+		{"tar", args{testDir("demozoo/test.tar"), "test.tar", uuid}, false},
+		{"tar.gz", args{testDir("demozoo/test.tar.gz"), "test.tar.gz", uuid}, false},
+		{"tar.bz2", args{testDir("demozoo/test.tar.bz2"), "test.tar.bz2", uuid}, false},
+		{"sz (snappy)", args{testDir("demozoo/test.tar.sz"), "test.tar.sz", uuid}, false},
+		{"xz", args{testDir("demozoo/test.tar.xz"), "test.tar.xz", uuid}, false},
+		{"zip", args{testDir("demozoo/test.zip"), "test.zip", uuid}, false},
+		{"zip.bz2", args{testDir("demozoo/test.bz2.zip"), "test.bz2.zip", uuid}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
