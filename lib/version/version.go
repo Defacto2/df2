8fdbe1c00c3f774e1ef9e072473309be5e56ce46
Ben Garrett <code.by.ben@gmail.com>
2020-08-09 14:33:14 +1000

    Removed most global vars.

--- lib/version/version.go
+++ lib/version/version.go
@@ -16,6 +16,6 @@ type Build struct {
 // B holds the build and version information.
 var B = Build{
 	Commit:  "n/a",
-	Date:    "2020-08-09T11:38:43+10:00",
-	Version: "1.1.6",
+	Date:    "2020-08-09T14:33:14+10:00",
+	Version: "1.1.7",
 }
