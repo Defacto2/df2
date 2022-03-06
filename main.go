3a8cfa9ec004c152771a3989c8f315ed21f60116
Ben Garrett <code.by.ben@gmail.com>
2022-03-07 09:14:55 +1100

    Updated (c) year.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-Copyright © 2021 Ben Garrett <code.by.ben@gmail.com>
+Copyright © 2021-22 Ben Garrett <code.by.ben@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
