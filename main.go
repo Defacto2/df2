aa364d9ff4c052d057d277276a950679547c9582
Ben Garrett <code.by.ben@gmail.com>
2022-06-08 11:36:37 +1000

    Tweaked requirements layout.

--- main.go
+++ main.go
@@ -50,7 +50,8 @@ func main() {
 	fs.Usage = func() {
 		// disable go flag help
 	}
-	if err := fs.Parse(os.Args[1:]); err != nil && !errors.As(err, &pflag.ErrHelp) {
+	if err := fs.Parse(os.Args[1:]); err != nil &&
+		!errors.As(err, &pflag.ErrHelp) {
 		log.Print(err)
 	}
 	if *ver || *v {
@@ -86,7 +87,7 @@ func app() string {
 }
 
 func exeTmp() string {
-	const tmp = ` ┌── requirements ─────────────┐
+	const tmp = ` ┌──── requirements ───────────┐
  │  database:     {{.Database}}  │
  │  ansilove:     {{.Ansilove}}  │
  │  webp lib:     {{.Webp}}  │
