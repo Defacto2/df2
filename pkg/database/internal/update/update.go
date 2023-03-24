52aa7e506d08a791a8cfda560452b3b08afa15b3
Ben Garrett <code.by.ben@gmail.com>
2023-03-24 13:13:25 +1100

    update tests and fixes.

--- pkg/database/internal/update/update.go
+++ pkg/database/internal/update/update.go
@@ -13,14 +13,15 @@ import (
 )
 
 var (
-	ErrDB      = errors.New("z database handle pointer cannot be nil")
+	ErrColumn  = errors.New("column value must be either platform or section")
+	ErrDB      = errors.New("database handle pointer cannot be nil")
 	ErrPointer = errors.New("pointer value cannot be nil")
 )
 
 // Update row values based on conditions.
 type Update struct {
-	Query string
-	Args  []any
+	Query string // Query is an SQL statement.
+	Args  []any  // Args are SQL statement values.
 }
 
 // Execute Query and Args to update the database and returns the total number of changes.
@@ -30,12 +31,12 @@ func (u Update) Execute(db *sql.DB) (int64, error) {
 	}
 	update, err := db.Prepare(u.Query)
 	if err != nil {
-		return 0, fmt.Errorf("update execute db prepare: %w", err)
+		return 0, fmt.Errorf("update execute prepare: %w", err)
 	}
 	defer update.Close()
 	res, err := update.Exec(u.Args...)
 	if err != nil {
-		return 0, fmt.Errorf("update execute db exec: %w", err)
+		return 0, fmt.Errorf("update execute exec: %w", err)
 	}
 	count, err := res.RowsAffected()
 	if err != nil {
@@ -54,9 +55,9 @@ const (
 
 // NamedTitles remove record titles that match the filename.
 func (col Column) NamedTitles(db *sql.DB, w io.Writer) error {
-	// if db == nil {
-	// 	return ErrDB
-	// }
+	if db == nil {
+		return ErrDB
+	}
 	if w == nil {
 		w = io.Discard
 	}
@@ -65,17 +66,17 @@ func (col Column) NamedTitles(db *sql.DB, w io.Writer) error {
 	if err != nil {
 		return err
 	}
-	result, err := tx.ExecContext(ctx, "UPDATE files SET record_title = \"\" WHERE files.record_title = ?", string(col))
+	res, err := tx.ExecContext(ctx, "UPDATE files SET record_title = \"\" WHERE files.record_title = ?", string(col))
 	if err != nil {
-		if err1 := tx.Rollback(); err1 != nil {
-			return err1
+		if err := tx.Rollback(); err != nil {
+			return err
 		}
 		return err
 	}
 	if err = tx.Commit(); err != nil {
 		return err
 	}
-	rows, err := result.RowsAffected()
+	rows, err := res.RowsAffected()
 	if err != nil {
 		return err
 	}
@@ -88,12 +89,19 @@ func (col Column) NamedTitles(db *sql.DB, w io.Writer) error {
 }
 
 // Distinct returns a unique list of values from the table column.
+// Column must be either platform or section.
 func Distinct(db *sql.DB, column string) ([]string, error) {
 	if db == nil {
 		return nil, ErrDB
 	}
-	var result string
-	rows, err := db.Query("SELECT DISTINCT ? AS `result` FROM `files` WHERE ? != \"\"", column, column)
+	if column != "platform" && column != "section" {
+		return nil, ErrColumn
+	}
+	stmt := "SELECT DISTINCT platform FROM `files` WHERE platform != \"\""
+	if column == "section" {
+		stmt = "SELECT DISTINCT section FROM `files` WHERE section != \"\""
+	}
+	rows, err := db.Query(stmt)
 	if err != nil {
 		return nil, fmt.Errorf("distinct query %q: %w", column, err)
 	}
@@ -101,6 +109,7 @@ func Distinct(db *sql.DB, column string) ([]string, error) {
 	if rows.Err() != nil {
 		return nil, rows.Err()
 	}
+	result := ""
 	values := []string{}
 	for rows.Next() {
 		if err := rows.Scan(&result); err != nil {
@@ -118,8 +127,9 @@ func Sections(db *sql.DB, w io.Writer, sections *[]string) error {
 	if sections == nil {
 		return fmt.Errorf("sections %w", ErrPointer)
 	}
-	var u Update
-	u.Query = "UPDATE files SET section=? WHERE `section`=?"
+	u := Update{
+		Query: "UPDATE files SET section=? WHERE `section`=?",
+	}
 	for _, s := range *sections {
 		u.Args = []any{strings.ToLower(s), s}
 		c, err := u.Execute(db)
@@ -135,8 +145,10 @@ func Sections(db *sql.DB, w io.Writer, sections *[]string) error {
 	}
 	// set all audio platform files to use intro section
 	// releaseadvert
-	u.Query = "UPDATE files SET section=? WHERE `platform`=?"
-	u.Args = []any{"releaseadvert", "audio"}
+	u = Update{
+		Query: "UPDATE files SET section=? WHERE `platform`=?",
+		Args:  []any{"releaseadvert", "audio"},
+	}
 	c, err := u.Execute(db)
 	if err != nil {
 		return fmt.Errorf("execute %w", err)
@@ -157,8 +169,9 @@ func Platforms(db *sql.DB, w io.Writer, platforms *[]string) error {
 	if platforms == nil {
 		return fmt.Errorf("platforms %w", ErrPointer)
 	}
-	var u Update
-	u.Query = "UPDATE files SET platform=? WHERE `platform`=?"
+	u := Update{
+		Query: "UPDATE files SET platform=? WHERE `platform`=?",
+	}
 	for _, p := range *platforms {
 		u.Args = []any{strings.ToLower(p), p}
 		c, err := u.Execute(db)
